760e8b287885db61f2599ccba5bb64142fbc4e06
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2022-03-08 15:16:02 +0900

    [add]header.php main.cssの追加

---
+++ header.php
@@ -0,0 +1,10 @@
+<header class="header">
+  <div class="header-wrapper">
+    <h1>bulletin borad app</h1>
+    <input class="search-box" type="search" name="search-word" placeholder="検索したいワードを入力してください">
+    <div class="links-box">
+      <a href="#" class="header-link">質問する</a>
+      <a href="#" class="header-link">ログイン</a>
+    </div>
+  </div>
+</header>
