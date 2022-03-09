65cc5b72dddf7f36df2e4490818ff8cb1c2bbcd9
津嶋勇輝 <67349483+tsushima3074@users.noreply.github.com>
2022-03-09 13:31:18 +0900

    not foundページの追加・詳細ページの追加

--- DetailQuestion.php
+++ DetailQuestion.php
@@ -1,3 +1,34 @@
 <?php
-  
+    require_once dirname(__FILE__).'/db/question.php';
+
+    // セッションの開始
+    session_start();
+
+    // if (isset($_SESSION["user_id"]) && $_SESSION["user_id"] !== "" &&
+    //     isset($_GET["question_id"]) && $_GET["question_id"] !== "" 
+    //   ) {
+      // $user_id = $_SESSION["user_id"];
+      $question_id = $_GET["question_id"];
+      $result = detailQuestion($question_id);
+      if(count($result) === 0){
+        header("Location:notFoundError.php");
+      }
+    // } else {
+    //   header("Location:index.php");
+    //   exit();
+    // }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    var_dump($result);
+  ?>
+</body>
+</html>
--- db/question.php
+++ db/question.php
@@ -5,12 +5,11 @@
     // 投稿詳細: detailQuestion
 
   function detailQuestion($question_id) {
-    function insertUserSql($sql) {
-      // 管理者用のコネクションの挿入
-      include dirname(__FILE__).'/userConnection.php';
-      try {
-        // sql分の構築
-        $sql = <<<eof
+    // 管理者用のコネクションの挿入
+    include dirname(__FILE__).'/userConnetcion.php';
+    try {
+      // sql分の構築
+      $sql = <<<EOF
 select q.question_id, q.question_title, q.question_detail, q.question_created, q.question_update, q.question_view,
 q_u.user_id, q_u.student_name,
 q_a.user_id, q_a.student_name,
@@ -23,24 +22,22 @@ left outer join (
   ) as q_a on q_a.user_id = a.user_id
 where 
 (q_a.user_id is not null or q.question_bestanswer = a.answer_id) and
-q.question_id = 1 and
+q.question_id = :id and
 q.delete_flag = 0 and
 q_u.user_id is not null
 order by a.answer_id = q.question_bestanswer desc, a.answer_date
+EOF ;
 
-eof
-
-        // pdoのインスタンス化
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':id', $question_id, PDO::PARAM_INT);
-        // 実行
-        $res = $stmt->execute();
-        // 実行結果の取り出し
-        $result = $stmt->fetchall(PDO::FETCH_ASSOC);
-        return $result;
-      } catch (PDOException $e) {
-        echo 'Connection failed: ' . $e->getMessage();
-        echo "<br>アカウントの作成に失敗しました。csvファイルの確認をお願いします";
-      }
+      // pdoのインスタンス化
+      $stmt = $pdo->prepare($sql);
+      $stmt->bindValue(':id', $question_id, PDO::PARAM_INT);
+      // 実行
+      $res = $stmt->execute();
+      // 実行結果の取り出し
+      $result = $stmt->fetchall(PDO::FETCH_ASSOC);
+      return $result;
+    } catch (PDOException $e) {
+      echo 'Connection failed: ' . $e->getMessage();
+      echo "<br>アカウントの作成に失敗しました。csvファイルの確認をお願いします";
     }
   }
