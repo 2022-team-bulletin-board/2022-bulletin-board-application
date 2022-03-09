4a5f380e970b2e98b411fea62876fa09d65566b2
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2022-03-09 15:00:15 +0900

    検索ボタンの追加、リンクの追加

--- header.php
+++ header.php
@@ -1,10 +1,20 @@
 <header class="header">
-  <div class="header-wrapper">
-    <h1>bulletin borad app</h1>
-    <input class="search-box" type="search" name="search-word" placeholder="検索したいワードを入力してください">
-    <div class="links-box">
-      <a href="#" class="header-link">質問する</a>
-      <a href="#" class="header-link">ログイン</a>
+    <div class="header-wrapper">
+      <h1>bulletin borad app</h1>
+      <div class="search-box">
+        <input type="search" name="question_id" placeholder="検索したいワードを入力してください">
+        <form action="#" method="GET" id="search-form">
+          <button type="submit" class="btn search">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+          
+        </form>
+      </div>
+      <div class="links-box">
+        <a href="#" class="header-link">質問する</a>
+        <a href="#" class="header-link">ユーザー情報</a>
+        <a href="#" class="header-link">ホーム</a>
+        <a href="#" class="header-link">ログアウト</a>
+      </div>
     </div>
-  </div>
-</header>
+  </header>
