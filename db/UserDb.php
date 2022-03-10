664e7e091ecc6bc74e74eecfab5d78eee3627cd2
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2022-03-10 14:53:23 +0900

    [add]ログイン機能に必要なdb操作をする関数の作成

--- db/UserDb.php
+++ db/UserDb.php
@@ -3,6 +3,7 @@
 
   // 現在の関数
   // ・登録処理: insertUserSql
+  // ・ログイン処理: selectUserSalt, selectUser
   
   // 新規ユーザー作成関数
   function insertUserSql($sql) {
@@ -21,11 +22,39 @@
   }
 
   // ログイン機能(mailをチェックして、あればソルトを返す)
-  function selectUserSalt($sql) {
+  function selectUserSalt($mail) {
     // ユーザー用のコネクションの挿入
-    include dirname(__FILE__).'/userConnection.php';
+    include dirname(__FILE__).'/userConnetcion.php';
     try {
-      
+      $sql = 'SELECT student_salt FROM users WHERE student_mail = :mail AND delete_flag = 0';
+      // pdoのインスタンス化
+      $stmt = $pdo->prepare($sql);
+      $stmt->bindValue(':mail', $mail, PDO::PARAM_STR);
+      // 実行
+      $res = $stmt->execute();
+      $result = $stmt->fetch(PDO::FETCH_ASSOC);
+      return $result;
+    } catch (PDOException $e) {
+      echo 'Connection failed: ' . $e->getMessage();
+    }
+  }
+
+  // ログイン機能(mailとハッシュ化したパスワードをチェックして、合致すればユーザーidと管理者フラグの値を返す)
+  function selectUser($mail,$hashPassword) {
+    // ユーザー用のコネクションの挿入
+    include dirname(__FILE__).'/userConnetcion.php';
+    try {
+      $sql = 'SELECT user_id,admin_flag FROM users WHERE student_mail = :mail AND student_pass = :pass';
+      // pdoのインスタンス化
+      $stmt = $pdo->prepare($sql);
+      $stmt->bindValue(':mail', $mail, PDO::PARAM_STR);
+      $stmt->bindValue(':pass', $hashPassword, PDO::PARAM_STR);
+      // 実行
+      $res = $stmt->execute();
+      $result = $stmt->fetch(PDO::FETCH_ASSOC);
+      return $result;
+    } catch (PDOException $e) {
+      echo 'Connection failed: ' . $e->getMessage();
     }
   }
   
