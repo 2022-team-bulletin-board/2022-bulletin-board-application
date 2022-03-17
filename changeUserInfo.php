355e2e86b4823ca18977274e2493dfb1af928543
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2022-03-17 11:19:48 +0900

    [add]必要なjs,cssファイルの読み込み/ヘッダーの表示

--- changeUserInfo.php
+++ changeUserInfo.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/changeUserInfo.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>ユーザー情報変更画面</title>
 </head>
 <body>
-    <h1>どちらの情報を変更しますか？</h1>
+    <?php include 'header.php';?>
+
+    <h1 class="center">どちらの情報を変更しますか？</h1>
     <button type="submit" id="name" class="submit">名前</button>
     <button type="submit" id="password" class="submit" span="20">パスワード</button>
 </body>
