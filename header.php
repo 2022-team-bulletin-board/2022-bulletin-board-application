3a2ea30f8a6edf33314bf53d95149ebef59f3ffb
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2022-03-17 11:56:33 +0900

    [fix]リンクの追記とログインしているかどうかのチェック処理

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
+<?php
+  // session_start();
+
+  // if(!isset($_SESSION['user_id']) && $_SESSION['user_id'] === '') {
+  //   header("Location:index.php");
+  // }
+?>
+
 <header class="header">
     <div class="header-wrapper">
       <h1>bulletin borad app</h1>
@@ -7,14 +15,13 @@
           <button type="submit" class="btn search">
             <i class="fa-solid fa-magnifying-glass"></i>
           </button>
-          
         </form>
       </div>
       <div class="links-box">
-        <a href="#" class="header-link">質問する</a>
-        <a href="#" class="header-link">ユーザー情報</a>
-        <a href="#" class="header-link">ホーム</a>
-        <a href="#" class="header-link">ログアウト</a>
+        <a href="./postQuestion.php" class="header-link">質問する</a>
+        <a href="./changeUserInfo.php" class="header-link">ユーザー情報</a>
+        <a href="./home.php" class="header-link">ホーム</a>
+        <a href="./logout.php" class="header-link">ログアウト</a>
       </div>
     </div>
   </header>
