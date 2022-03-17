cee90d8c01c1350e0dc418f474fd8002e3d29422
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2022-03-17 10:55:43 +0900

    [add]phpファイルの作成/仮ボタンの設置

---
+++ changeUserInfo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ユーザー情報変更画面</title>
+</head>
+<body>
+    <button type="submit" id="name" class="submit"><i class="fa fa-long-arrow-right"></i></button>
+    <button type="submit" id="password" class="submit"><i class="fa fa-long-arrow-right"></i></button>
+</body>
+</html>
