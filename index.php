66764ec4d40d14d6b2bea5a547dba1cf529feac1
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2022-03-22 13:53:03 +0900

    [delete]不要なファイルの読み込みを削除

--- index.php
+++ index.php
@@ -63,7 +63,6 @@
     <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="./css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script type="text/javascript" src="js/login.js"></script>
     <title>morijyobi</title>
 </head>
 
