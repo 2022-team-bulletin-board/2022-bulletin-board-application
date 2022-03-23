7f290d8401589c0580bbc78e97120d7a685808dd
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2022-03-23 14:11:12 +0900

    [add]パスワード変更用のフォーム作成/変更を掛けるロジックの作成

--- changeUserInfo.php
+++ changeUserInfo.php
@@ -1,11 +1,26 @@
 <?php
     session_start();
 
-    if(isset($_SESSION["user_id"]) && $_SESSION["admin_id"] == true){
+    require_once dirname(__FILE__).'/db/UserDb.php';
+    include 'header.php';
 
-    } else {
-        header('Location: index.php');
+    try{
+        if(isset($_SESSION["user_id"]) && $_SESSION["admin_id"] == true){
+            if(isset($_POST["changedName"])){
+                $change_name_result = updateName($_POST["changedName"], $_SESSION["user_id"]);
+                if($change_name_result){
+                    echo "名前の変更に成功しました";
+                } else {
+                    echo "名前の変更に失敗しました。<br>もう一度お願いいたします。";
+                }
+            }
+        } else {
+            header('Location: index.php');
+        }
+    } catch(Exception $e){
+        echo $e;
     }
+    
 ?>
 
 
@@ -22,25 +37,38 @@
     <title>ユーザー情報変更画面</title>
 </head>
 <body>
-    <?php include 'header.php';?>
 
     <h1 class="center">どちらの情報を変更しますか？</h1>
     <button type="submit" id="name" class="submit">名前</button>
     <button type="submit" id="password" class="submit">パスワード</button>
 
-    <div id="grey" class="grey">
-        <div id="popup" class="popup">
-        <button type="button" id="clearBtn" class="clearBtn">X</button>
-            <form action="#" method="POST" class="changeNameForm">
+    <div id="nameContainer" class="nameContainer">
+        <div id="namePopup" class="namePopup">
+        <button type="button" id="clearNameBtn" class="clearNameBtn">X</button>
+            <form action="changeUserInfo.php" method="POST" id="changeNameForm" class="changeNameForm">
                 <div>
-                    <label for="nameText">変更後の名前</label>
-                    <input type="text" id="changedName" placeholder="盛岡ジョビ男">
-                    <input type="submit" id="changeBtn" value="名前を変更" class="change-btn">
+                    <label for="changedName">変更後の名前</label>
+                    <input type="text" id="changedName" name="changedName" placeholder="盛岡ジョビ男">
+                    <input type="button" id="changeNameBtn" value="名前を変更" class="change-name-btn">
                 </div>
             </form>
         </div>
     </div>
-    
+
+    <div id="pwContainer" class="pwContainer">
+        <div id="pwPopup" class="pwPopup">
+        <button type="button" id="clearPwBtn" class="clearPwBtn">X</button>
+            <form action="./func/editPass.php" method="POST" id="changePwForm" class="changePwForm">
+                <div>
+                    <label for="changedPwFirst">変更後のパスワード:一回目</label>
+                    <input type="text" id="changedPwFirst" name="changedPwFirst">
+                    <label for="changedPwSecond">変更後のパスワード:二回目</label>
+                    <input type="text" id="changedPwSecond" name="changedPwSecond">
+                    <input type="submit" id="changePwBtn" value="パスワードを変更" class="change-pw-btn">
+                </div>
+            </form>
+        </div>
+    </div>
 
 </body>
 </html>
