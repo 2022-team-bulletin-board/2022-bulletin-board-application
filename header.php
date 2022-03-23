5de8b86719a6bccd291cb08a3dfc747a3a84af0d
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2022-03-23 11:37:16 +0900

    [change]ヘッダーのレスポンシブ化

--- header.php
+++ header.php
@@ -21,24 +21,22 @@
         </div>
       </div>
     </div>
-    <div class="sp-nav">
-
-      <nav class="drawer-content">
-        <ul class="drawer-menu">
-          <li class="drawer-menu-item">
-            <a href="./postQuestion.php">質問する</a>
-          </li>
-          <li class="drawer-menu-item">
-            <a href="./changeUserInfo.php">ユーザー情報</a>
-          </li>
-          <li class="drawer-menu-item">
-            <a href="./home.php">ホーム</a>
-          </li>
-          <li class="drawer-menu-item">
-            <a href="./logout.php">ログアウト</a>
-          </li>
-        </ul>
-      </nav>
+    <div class="header-bottom">
+      <div class="search-box sp">
+        <form action="searchQuestion.php" method="GET" id="search-form">
+          <input type="search" name="search_word" placeholder="検索したいワードを入力してください">
+          <button type="submit" class="btn search">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </form>
+      </div>
+      <div class="links-box sp">
+          <a href="./postQuestion.php" class="header-link">質問する</a>
+          <a href="./changeUserInfo.php" class="header-link">ユーザー情報</a>
+          <a href="./home.php" class="header-link">ホーム</a>
+          <a href="./logout.php" class="header-link">ログアウト</a>
+      </div>
     </div>
+    <script src="./js/jquery-3.6.0.min.js"></script>
     <script src="./js/header.js"></script>
   </header>
--- js/header.js
+++ js/header.js
@@ -2,6 +2,14 @@ const sp_nav = document.querySelector('.sp-nav');
 const menu_btn = document.querySelector('.menu-btn');
 
 menu_btn.addEventListener('click',() => {
-  menu_btn.classList.toggle('active');
-  sp_nav.classList.toggle('active');
+  // menu_btn.classList.toggle('active');
+  // sp_nav.classList.toggle('active');
+
+  $('.header-bottom').slideToggle();
+})
+
+window.addEventListener('resize',() => {
+  if(window.innerWidth >= 1024) {
+    $('.header-bottom').hide();
+  }
 })
