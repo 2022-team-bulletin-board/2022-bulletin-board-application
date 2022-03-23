48e74220be8e11456c272e9fa49bc93b7c7f7f1d
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2022-03-23 14:59:35 +0900

    [fix]session_start関数が重複していたので修正

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
     require_once dirname(__FILE__).'/db/UserDb.php';
     require_once dirname(__FILE__).'/func/UsersFunc.php';
 
-    session_start();
-
     class NotFoundValue extends Exception {
         public function __construct(){
             $this->message = '<p class="error-text">ログインに失敗しました。<br>メールアドレスかパスワードが間違っています。</p>';
