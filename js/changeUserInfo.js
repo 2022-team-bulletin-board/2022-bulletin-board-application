f556c2771424c5e266572d58994a3dc162add18d
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2022-03-23 10:16:29 +0900

    [add]必要なhtml要素の宣言とボタンクリック時の処理追加

--- js/changeUserInfo.js
+++ js/changeUserInfo.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    nameButton = document.getElementById('name');
-    passwordButton = document.getElementById('password');
-    console.log('test');
+    const nameButton = document.getElementById('name');
+    const passwordButton = document.getElementById('password');
+    const grey = document.getElementById('grey');
+    const popup = document.getElementById('popup');
+    const clearBtn = document.getElementById('clearBtn');
+    const changeBtn = document.getElementById('changeBtn');
+    const changedName = document.getElementById('changedName');
+    let changedNameText = "";
+
+    // console.log(nameButton);
+    
+    nameButton.addEventListener('click', ()=> {
+        grey.classList.add('show');
+    })
+
+    clearBtn.addEventListener('click', () => {
+        grey.classList.remove('show');
+    })
+
+    changeBtn.addEventListener('click', () => {
+        changedNameText = changedName.value;
+        console.log(changedNameText);
+    })
+
 });
